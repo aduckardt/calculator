0a1f47ecfcf8f7385987ef2e355ea9493b7e1155
Alexander Duckardt <aduckardt@gmail.com>
2017-05-05 17:29:04 -0400

    intial app structure

---
+++ src/main/java/calculator/Main.java
@@ -0,0 +1,15 @@
+package calculator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by Alexander Dukkardt on 2017-05-05.
+ */
+public class Main {
+    private static Logger log = LoggerFactory.getLogger(Main.class);
+   
+    public static void main(String[] args){
+        log.info("This is calculator app");
+    }
+}
